268aca61443489331c8beef2bf802c61a8b88e03
Padmalatha routhu <routhupadmalatha@gmail.com>
2026-02-06 18:50:32 +0530

    Initial commit

---
+++ Age.py
@@ -0,0 +1,9 @@
+n = int(input("Enter your Age : "))
+if n<18:
+    print("You are a Child")
+elif n <30:
+    print("You are Young")
+elif n<50:
+    print("You are Middle")
+else:
+    print("You are Old")        
---
+++ Arithmatic.py
@@ -0,0 +1,12 @@
+Var1 = int(input("Ente a number :"))
+Var2 = int(input("Enter another number :"))
+Addition = Var1 + Var2
+Subtraction = Var1 - Var2
+Multiplication = Var1* Var2
+Division = Var1/Var2
+Remainder = Var1%Var2
+print(Addition)
+print(Subtraction)
+print(Multiplication)
+print(Division)
+print(Remainder)
